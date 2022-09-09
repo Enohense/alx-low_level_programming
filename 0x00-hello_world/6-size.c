15fc48d2c9b2c52ac990586d0c1025b580884dfa
Enohense <williamssandra803@gmail.com>
2022-09-08 19:52:26 -0700

    A lot of printf

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
 
 /**
-* main - Entry point
+* main - print the string in the put function
 *
-* Return: Always 0 (Success)
+* Description: using the main function
+* this program prints "Programming is like a multilingual puzzle
+* Return: 0
 */
 int main(void)
 {
-int a;
-long int b;
-long long int c;
-char d;
-float f;
-
-printf("Size of a char: %lu byte(s)\n", (unsighed long)sizeof(d));
-printf("Size of an int: %lu byte(s)\n", (unsighed long)sizeof(a));
-printf("Size of a long int: %lu byte(s)\n", (unsighed long)sizeof(b));
-printf("Size of a long long int: %lu byte(s)\n", (unsighed long)sizeof(c));
-printf("Size of a float: %lu byte(s)\n", (unsighed long)sizeof(f));
-return (0);
-}
+	char a;
+	int b;
+	long c;
+	long long d;
+	float f;
+	
+	printf("Size of a char: %ld byte(s)\n", sizeof(a));
+	printf("Size of an int: %ld byte(s)\n", sizeof(b));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(c));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(d));
+	printf("Size of a float: %ld byte(s)\n", sizeof(f));
+	return (0);
