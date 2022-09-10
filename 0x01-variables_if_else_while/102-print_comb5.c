5f9cfa70a9f6a2d80809b02a8f0d9c8c6504984a
Enohense <williamssandra803@gmail.com>
2022-09-09 21:41:21 -0700

    Two two-digits

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -23,11 +23,11 @@ int main(void)
 				putchar((y / 10) + '0');
 				putchar((y % 10) + '0');
 
-				if (x == 98 && y == 99)
-					continue;
-
-				putchar(',');
-				putchar(' ');
+				if (x != 98)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
