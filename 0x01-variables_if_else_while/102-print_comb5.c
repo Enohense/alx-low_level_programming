90a73c792ca1c1b29ea31078fed415acae5f4f54
Enohense <williamssandra803@gmail.com>
2022-09-09 21:32:10 -0700

    Two two-digits

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+* main - entry point
+*
+* codes for printing all possible combination of two two-digits
+* in ascending order and separated by a comma followed by a space
+*
+* Return: 0
+*/
+int main(void)
+{
+	int x, y;
+
+	for (x = 0; x < 100; x++)
+	{
+		for (y = 0; y < 100; y++)
+		{
+			if (x > y)
+			{
+				putchar((x / 10) + '0');
+				putchar((x % 10) + '0');
+				putchar(' ');
+				putchar((y / 10) + '0');
+				putchar((y % 10) + '0');
+
+				if (x == 98 && y == 99)
+					continue;
+
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
