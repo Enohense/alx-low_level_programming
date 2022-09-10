df57d6df0b85811e962ea8bdb81c3cf5431ce8b3
Enohense <williamssandra803@gmail.com>
2022-09-09 21:07:48 -0700

    Brain and material combo

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+* main - entry point
+*
+*codes for printing all possible combination of two digits
+* in ascending order and seperated by a comma followed by a space
+*
+* Return: 0
+*/
+int main(void)
+{
+	int x, y;
+
+	for (x = 0; x < 9; x++)
+	{
+		for (y = x + 1; y < 10; y++)
+		{
+			if (y > x)
+			{
+				putchar((x % 10) + '0');
+				putchar((y % 10) + '0');
+
+				if (x == 8 && y == 9)
+					continue;
+
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
