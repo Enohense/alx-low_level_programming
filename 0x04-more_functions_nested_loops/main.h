b0feccd57efddfae1d4c4c1ffec28e561379ca70
Enohense <111037842+Enohense@users.noreply.github.com>
2022-09-16 01:45:42 +0100

    Task 0

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,12 +1,10 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef FILE_MAIN
+#define FILE_MAIN
 
-/**
-*main - header file for prototypes
-*void prototypes (void)
-*int prototypes (int)
-*/
-
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
@@ -14,10 +12,6 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
-void print_numbers(int n);
-int _putchar(char);
-int _isupper(int);
-int _isdigit(int);
-int mul(int, int);
+void print_number(int n);
 
-#endif /* MAIN_H */
+#endif
